6004741a3d8c7067f78b2ef8edf57ea2e50cdcca
Ivan-Mac pro m4 pro <vanya.d.pro56@gmail.com>
2026-08-05 23:29:26 +0300

    homework_

--- module_12_multitasking_2/homework/hw2/main.py
+++ module_12_multitasking_2/homework/hw2/main.py
@@ -1,10 +1,37 @@
-def process_count(username: str) -> int:
+import os
+import subprocess
+
+def process_count(username: str):
     # количество процессов, запущенных из-под
     # текущего пользователя username
-    pass
+    try:
+        output = subprocess.run(
+            ['pgrep', '-u', username], stderr=subprocess.DEVNULL, stdout=subprocess.PIPE, text=True
+        )
+        return len(output.stdout.splitlines())
+    except Exception as e:
+        print(e)
 
 
 def total_memory_usage(root_pid: int) -> float:
     # суммарное потребление памяти древа процессов
     # с корнем root_pid в процентах
-    pass
+
+    cmd = f"""
+            total=$(grep MemTotal /proc/meminfo | awk '{{print $2}}')
+            sum=$(ps -eo pid,ppid,rss | awk -v root={root_pid} '
+                function s(p) {{ t=rss[p]; for(c in children[p]) t+=s(c); return t }}
+                {{ rss[$1]=$3; children[$2][$1]=1 }}
+                END {{ print s(root) }}
+            ')
+            echo "scale=2; $sum * 100 / $total" | bc
+        """
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    output = result.stdout.strip()
+    return float(output) if output else 0.0
+
+
+if __name__ == '__main__':
+    print(process_count("luidvikovivanolegovich"))
+    print(total_memory_usage(os.getpid()))
+
--- module_12_multitasking_2/homework/hw3/main.py
+++ module_12_multitasking_2/homework/hw3/main.py
@@ -1,30 +1,39 @@
-from threading import Semaphore, Thread
+from threading import Semaphore, Thread, Event
 import time
 
-sem: Semaphore = Semaphore()
+sem = Semaphore()
+stop_event = Event()
 
 
 def fun1():
-    while True:
-        sem.acquire()
-        print(1)
-        sem.release()
+    while not stop_event.is_set():
+
+        if sem.acquire(timeout=0.1):
+            print(1)
+            sem.release()
         time.sleep(0.25)
 
 
 def fun2():
-    while True:
-        sem.acquire()
-        print(2)
-        sem.release()
+    while not stop_event.is_set():
+        if sem.acquire(timeout=0.1):
+            print(2)
+            sem.release()
         time.sleep(0.25)
 
 
-t1: Thread = Thread(target=fun1)
-t2: Thread = Thread(target=fun2)
+t1 = Thread(target=fun1)
+t2 = Thread(target=fun2)
+
 try:
     t1.start()
     t2.start()
+
+    while True:
+        time.sleep(1)
 except KeyboardInterrupt:
     print('\nReceived keyboard interrupt, quitting threads.')
-    exit(1)
+    stop_event.set()
+    t1.join()
+    t2.join()
+    print('Threads finished.')
